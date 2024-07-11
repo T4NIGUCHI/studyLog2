132de20f82044be7b4d610cda937ba1ef33e097e
T4niguchi <tech@noizless.co.jp>
2024-07-11 10:49:28 +0900

    0711

---
+++ src/App.jsx
@@ -0,0 +1,130 @@
+import { useState, useEffect } from "react";
+import "./styles.css";
+import { createClient } from '@supabase/supabase-js';
+import { app } from "./firebase";
+
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+
+const supabase = createClient(supabaseUrl, supabaseAnonKey);
+
+export const App = () => {
+  const [records, setRecords] = useState([]);
+  const [inputTitle, setTitle] = useState("");
+  const [inputTime, setTime] = useState("");
+  const [error, setError] = useState("");
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const { data, error } = await supabase
+          .from('study-record') 
+          .select('title, time');
+
+        if (error) {
+          console.error("Error fetching data:", error);
+        } else {
+          setRecords(data);
+        }
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      } finally {
+        setIsLoading(false); 
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  const handleInputTitle = (event) => {
+    setTitle(event.target.value);
+    if (inputTitle !== "" && inputTime !== "") {
+      setError("");
+    }
+  };
+
+  const handleInputTime = (event) => {
+    if (/^\d*$/.test(event.target.value)) {
+      setTime(event.target.value);
+      if (inputTitle !== "" && inputTime !== "") {
+        setError("");
+      }
+    }
+  };
+
+  const handleSetRecords = async () => {
+    if (inputTitle === "" || inputTime === "") {
+      setError("入力されていない項目があります");
+    } else {
+      const newRecord = { title: inputTitle, time: inputTime };
+      const { error } = await supabase
+        .from('study-record') 
+        .insert([newRecord]);
+
+      if (error) {
+        console.error("Error inserting data:", error);
+      } else {
+        setRecords([...records, newRecord]);
+        setTitle("");
+        setTime("");
+        setError("");
+      }
+    }
+  };
+
+  const handleDeleteRecord = async (record) => {
+    const { error } = await supabase
+      .from('study-record')
+      .delete()
+      .eq('title', record.title)
+      .eq('time', record.time);
+
+    if (error) {
+      console.error("Error deleting record:", error);
+    } else {
+      const updatedRecords = records.filter(item => item !== record);
+      setRecords(updatedRecords);
+    }
+  };
+
+  const sumTime = records.reduce((sum, record) => sum + parseInt(record.time, 10), 0);
+
+  if (isLoading) {
+    return <div className="App">Loading...</div>;
+  }
+
+  return (
+    <div className="App">
+      <>
+        <div>
+          学習内容 <input value={inputTitle} onChange={handleInputTitle} />
+          <p />
+          学習時間 <input value={inputTime} onChange={handleInputTime} /> 時間
+        </div>
+
+        <div>
+          入力されている学習内容:{inputTitle}
+          <p />
+          入力されている時間:{inputTime}
+          <p />
+          <button onClick={handleSetRecords}>登録</button>
+          <p>{error}</p>
+        </div>
+
+        <div>
+          <ul>
+            {records.map((record, index) => (
+              <li key={index}>
+                {record.title} {record.time}時間
+                <button onClick={() => handleDeleteRecord(record)}>削除</button>
+              </li>
+            ))}
+          </ul>
+        </div>
+
+        <p>合計時間：{sumTime}時間</p>
+      </>
+    </div>
+  );
+};
---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAcZpqsQV8KYTXDx3Kszt_8QXjxgaRcNJ8",
+  authDomain: "fbdeploytest-b4e12.firebaseapp.com",
+  projectId: "fbdeploytest-b4e12",
+  storageBucket: "fbdeploytest-b4e12.appspot.com",
+  messagingSenderId: "358875890495",
+  appId: "1:358875890495:web:5443ae4067d04a7f7c498a"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
